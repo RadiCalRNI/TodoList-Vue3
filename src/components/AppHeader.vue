06cfcd10900af0b0dd5ea6f88d656e07a3cb838e
Mohammad Rouhani <m.rouhani2001@gmail.com>
2023-02-09 16:12:46 +0330

    Change theme is applied, when theme is dark sun icon appear and when theme is light moon icon apear

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,40 @@
 <template>
   <header class="card">
     <h1>لیست وظایف</h1>
-    <button id="theme-switcher">
-      <img src="../assets/images/icon-sun.svg" alt="Change color theme" />
+    <button id="theme-switcher" @click="changeTheme">
+      <img
+        v-if="defaultTheme == 'dark'"
+        src="../assets/images/icon-sun.svg"
+        alt="Change color theme"
+      />
+      <img
+        v-else
+        src="../assets/images/icon-moon.svg"
+        alt="Change color theme"
+      />
     </button>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      defaultTheme: "dark",
+    };
+  },
+  methods: {
+    changeTheme() {
+      if (this.defaultTheme == "dark") {
+        this.defaultTheme = "light";
+        document.body.classList.add("light");
+      } else {
+        this.defaultTheme = "dark";
+        document.body.classList.remove("light");
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
